29391af0a5ab101b6cb17c24e5b3e9d051e55cb8
SimoneColombo1 <simcolombo2@gmail.com>
2024-06-02 18:27:09 +0200

    Creato l'array di oggetti

---
+++ JS/script.js
@@ -0,0 +1,57 @@
+//?Vue
+const { createApp }= Vue;
+createApp({
+data(){
+
+return{
+ ToDoList:[{ titolo:'Spesa',
+             testo:'Pere, Banane, Arancie, Bicarbonato di sodio,Acqua oligominerale senza sali aggiunti, Sale iodato marino',
+             done:false
+ },
+ 
+ {  titolo:'Film da vedere',
+     testo:'la vita è bella, barbie, One Piece Red il film, Mission Impossible 4: protocollo fantasma,Fast & Furios: tokyo drift, la storia infinita ' ,
+     done:false
+ },
+
+ {  titolo: 'giochi da giocare',
+    testo: 'elden Ring shadown of the erdtree,elden Ring shadown of the erdtree,elden Ring shadown of the erdtree,elden Ring shadown of the erdtree,elden Ring shadown of the erdtree,elden Ring shadown of the erdtree,elden Ring shadown of the erdtree,elden Ring shadown of the erdtree,elden Ring shadown of the erdtree,elden Ring shadown of the erdtree,elden Ring shadown of the erdtree,elden Ring shadown of the erdtree,elden Ring shadown of the erdtree,elden Ring shadown of the erdtree,elden Ring shadown of the erdtree,elden Ring shadown of the erdtree,elden Ring shadown of the erdtree,',
+    done:false
+
+
+ }
+
+ 
+
+ ]
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+})
